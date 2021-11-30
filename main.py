1e9ce81b5e857016d6f7ba77c1f39cc9a43ff366
oliiiiiiiiiiiii <mullickneeladri@gmail.com>
2021-11-30 19:02:36 +0530

    felt like commiting again

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from json.decoder import JSONDecodeError
-import httpx
-import json
+from httpx import get
 
 class poke_client:  
 
@@ -8,12 +7,12 @@ class poke_client:
 
     def poke_name_by_id(self,number:int)->str:
         try:
-            return httpx.get(f"{poke_client.base_url}pokemon/{str(number)}").json()["name"]
-        except json.decoder.JSONDecodeError:
+            return get(f"{poke_client.base_url}pokemon/{str(number)}").json()["name"]
+        except JSONDecodeError:
             return "No Such Pokemon with that id"
 
     def poke_id_by_name(self,name:int)->str:
         try:
-            return httpx.get(f"{poke_client.base_url}pokemon/{str(name)}").json()['id']
-        except json.decoder.JSONDecodeError:
+            return get(f"{poke_client.base_url}pokemon/{str(name)}").json()['id']
+        except JSONDecodeError:
             return "No Such Pokemon with that name"
