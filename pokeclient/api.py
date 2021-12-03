32a9cb725e028b99fda9866a95dc6a7d7ddd4e0b
Oliiiii <74825334+oliiiiiiiiiiiii@users.noreply.github.com>
2021-12-03 15:48:40 +0530

    using self

--- pokeclient/api.py
+++ pokeclient/api.py
@@ -35,6 +35,6 @@ class PokeClient:
 
 
     def random_pokemon(self) -> Optional[Pokemon]:
-        data = httpx.get(f"{PokeClient.base_url}pokemon?limit=1118").json()
+        data = httpx.get(f"{self.base_url}pokemon?limit=1118").json()
         data = [data.get('results')[_].get('name') for _ in range(1118)]
         return self.fetch_pokemon(__import__('random').choice(data))
