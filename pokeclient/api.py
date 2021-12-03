ba46816de0921d1d2117d5cd8bcf67f5047cabd8
Oliiiii <74825334+oliiiiiiiiiiiii@users.noreply.github.com>
2021-12-03 10:45:13 +0000

    reduce usage of same  lines of code more than once

--- pokeclient/api.py
+++ pokeclient/api.py
@@ -3,6 +3,7 @@ import httpx
 from .pokemon import Pokemon
 from typing import Union, Optional
 from .errors import PokeNotFound
+import random
 
 
 class PokeClient:
@@ -16,25 +17,20 @@ class PokeClient:
                     address = int(poke)
                 except ValueError:
                     address = poke.lower()
-            data = httpx.get(f"{PokeClient.base_url}pokemon/{address}").json()
+            else:
+                address = str(poke)
+            data = httpx.get(f"{self.base_url}pokemon/{address}").json()
             return Pokemon(data)
         except json.decoder.JSONDecodeError:
             raise PokeNotFound
 
     def can_move(self, poke: Union[str, int], move: str) -> Optional[bool]:
-        try:
-            if isinstance(poke, str):
-                try:
-                    address = int(poke)
-                except ValueError:
-                    address = poke.lower()
-            data = httpx.get(f"{PokeClient.base_url}pokemon/{address}").json()
-            return move.lower() in Pokemon(data).moves
-        except json.decoder.JSONDecodeError:
-            raise PokeNotFound
-
+        data = self.fetch_pokemon(poke)
+        return move.lower() in data.moves
 
     def random_pokemon(self) -> Optional[Pokemon]:
-        data = httpx.get(f"{self.base_url}pokemon?limit=1118").json()
-        data = [data.get('results')[_].get('name') for _ in range(1118)]
-        return self.fetch_pokemon(__import__('random').choice(data))
+        data = [
+            httpx.get(f"{self.base_url}pokemon?limit=1118").json().get(
+                'results')[_].get('name') for _ in range(1118)
+        ]
+        return self.fetch_pokemon(random.choice(data))
--- test.py
+++
@@ -1,4 +0,0 @@
-from pokeclient.api import PokeClient
-
-
-print(a:=PokeClient().random_pokemon())
