f82d728be8b5cdd32d0ffca69d4f00f38a1fde9c
Oliiiii <74825334+oliiiiiiiiiiiii@users.noreply.github.com>
2021-12-06 18:26:36 +0530

    Delete pokeclient directory

--- pokeclient/__init__.py
+++
@@ -1,7 +0,0 @@
-from .api import *
-from .pokemon import *
-from .errors import *
-
-__title__ = "pokeclient"
-__author__ = "oliiiiiiiiiiiii"
-__version__ = "1.0.0"
--- pokeclient/errors.py
+++
@@ -1,7 +0,0 @@
-class PokeNotFound(Exception):
-  def __init__(self):
-    super().__init__("No such pokemon found")
-
-class MoveNotFound(Exception):
-  def __init__(self):
-    super().__init__("No such move found")
