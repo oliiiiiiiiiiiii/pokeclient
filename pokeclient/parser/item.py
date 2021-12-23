0c900198f8e277a40ddcd8382ef7062147e84478
oliiiiiiiiiiiii <mullickneeladri@gmail.com>
2021-12-23 12:52:07 +0530

    item parsing enhanced

--- pokeclient/parser/item.py
+++ pokeclient/parser/item.py
@@ -2,6 +2,35 @@ from dataclasses import dataclass
 from payload import DataPayload
 from ..utility.common_models import Name
 from evolution_chain import EvolutionChain
+from item_category import ItemCategory
+from version import Version
+from item_fling_effect import ItemFlingEffect
+from item_attribute import ItemAttribute
+@dataclass(frozen=True)
+class ItemHolderPokemonVersionDetail:
+
+    data : DataPayload
+
+    @property
+    def rarity(self):
+        return self.data.get("rarity")
+
+    @property
+    def version(self):
+        return Version(self.data.get("version"))
+
+@dataclass(frozen=True)
+class ItemHolderPokemon:
+
+    data : DataPayload
+
+    @property
+    def pokemon(self):
+        return self.data.get("pokemon")
+
+    @property
+    def version_details(self):
+        return ItemHolderPokemonVersionDetail(self.data.get("version_details"))
 
 @dataclass(frozen=True)
 class ItemSprite:
@@ -46,15 +75,26 @@ class Item:
     def baby_trigger_for(self):
         return EvolutionChain(self.data.get("baby_trigger_for"))
 
+    @property
+    def category(self):
+        return ItemCategory(self.data.get("category"))
+
+    @property
+    def held_by_pokemon(self):
+        return ItemHolderPokemon(self.data.get("held_by_pokemon"))
+
+    @property
+    def fling_effect(self):
+        return ItemFlingEffect(self.data.get("fling_effect"))
+
+    @property
+    def attributes(self):
+        return [ItemAttribute(_) for _ in self.data.get("attributes")]
 
 '''
 TODO:
-fling_effect
-attributes
-category
 effect_entries
 flavor_text_entries
 game_indices
-held_by_pokemon
 machines
 '''
